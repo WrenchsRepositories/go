7715e21b02ca745330235bb92bd7d3b310c78d18
wrench <amichelangelo@outlook.com>
2026-09-10 23:41:26 +0800

    modify hello.go

--- hello.go
+++ hello.go
@@ -2,5 +2,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	var hello string = "hello"
+	langage_name := "go"
+	var pi float32 = 3.14
+	fmt.Println(hello + " " + langage_name)
+	fmt.Printf("pi is %.2f\n", pi)
 }
